c6590eac0ade33ee726c56a1b53d5cacfd59176b
Chiradeep-Banik <chiradeep13@gmail.com>
2021-07-20 00:08:14 +0530

    Finished project 5

--- JS_Project_5/script.js
+++ JS_Project_5/script.js
@@ -1,4 +1,24 @@
-var burger = document.querySelector('#burger');
-burger.onclick = ()=>{
-    document.body.classList.toggle('.asd');
+var toggle_bar = document.querySelector(".toggle");
+var items = document.querySelectorAll(".item");
+var other_items = document.querySelectorAll(".other_item");
+
+toggle_bar.onclick = ()=>{
+    if(window.innerWidth < 768 && window.innerWidth > 568){
+        for(var i = 0; i < other_items.length; i++){
+            if(other_items[i].style.display == "block"){
+                other_items[i].style.display = "none";
+            }else{
+                other_items[i].style.display = "block";
+            }
+        }
+    }
+    if(window.innerWidth < 568){
+        for(var i = 0; i < items.length; i++){
+            if(items[i].style.display == "block"){
+                items[i].style.display = "none";
+            }else{
+                items[i].style.display = "block";
+            }
+        }
+    }
 }
